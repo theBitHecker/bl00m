2b47e26e44c34d9d726176a3bd27f4de577177d2
noah <noah.jabir@gmail.com>
2026-02-10 17:50:48 -0500

    added damage and die functions

--- resources/plants/resource_templates/plant.gd
+++ resources/plants/resource_templates/plant.gd
@@ -8,6 +8,7 @@ class_name Plant
 @export var growth_stages: Array = [-66.7, -33.3]
 @export var low_health: float = 20
 @export var growth_loss_per_damage = 1
+@export var water_flow_usage: float = 10.0
 
 
 @export_group("Cost")
@@ -15,8 +16,8 @@ class_name Plant
 @export var sunlight_cost: int = 0
 @export var metal_cost: int = 0
 
-func grow(plant: Node2D, delta: float):
-	plant.growth += growth_speed_per_sec*delta
+func grow(plant: Node2D, delta: float, growth_multiplier: float):
+	plant.growth += growth_speed_per_sec*delta*growth_multiplier
 	plant.growth = clampf(plant.growth, min_growth, max_growth)
 	for growth_stage_idx in range(len(growth_stages)+1):
 		var growth_stage_bounds = []
@@ -37,10 +38,20 @@ func grow(plant: Node2D, delta: float):
 		plant.get_node("Sprite").play("base")
 		plant.life_stage = "maturing"
 		
-func damage(plant, amount):
-	pass
-		
-		
-			
-		
-			
+
+func damage(plant: Node2D, amount: float):
+	plant.growth -= amount * growth_loss_per_damage
+	if plant.life_cycle == "growing":
+		die(plant)
+	plant.life_cycle = "matured"
+	if plant.growth >= 0 and plant.growth <= low_health and plant.life_stage == "matured":
+		plant.get_node("Sprite").play("low")
+	elif plant.growth == max_growth:
+		plant.get_node("Sprite").play("full")
+	else:
+		plant.get_node("Sprite").play("base")
+
+func die(plant: Node2D):
+	var map = plant.get_parent()
+	map.set_tile(plant.tile_pos, -1)
+	plant.queue_free()
