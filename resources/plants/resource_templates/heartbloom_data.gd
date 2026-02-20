473b733e1e68835b7dc661cede5ce2a3f1bf5506
Noah Jabir <noah.jabir@gmail.com>
2026-02-20 14:54:41 -0500

    added sounds folder and 10 sounds and 4 musics

--- resources/plants/resource_templates/heartbloom_data.gd
+++ resources/plants/resource_templates/heartbloom_data.gd
@@ -1,11 +1,6 @@
 extends PlantData
 class_name HeartbloomData
 
-@export_group("Starting Resources")
-@export var starting_water: int
-@export var starting_sunlight: int
-@export var starting_metal: int
-
 @export_group("Heartbloom Only")
 @export var build_radius: int
 @export var dehydration_immune: bool
