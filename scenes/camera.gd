c0a772192b0c7ecd9b8bd13be410aa058f6880f9
Noah Jabir <noahj64@nycstudents.net>
2026-04-28 16:50:45 -0400

    fixed cam, and made heartbloom

--- scenes/camera.gd
+++ scenes/camera.gd
@@ -2,9 +2,6 @@ extends Camera2D
 
 func _process(delta):
 	var input_vector = Input.get_vector("left", "right", "up", "down")
-	position += input_vector*delta*1000
-	var camera_input = (2**(Input.get_axis("zoom out", "zoom in"))*2
-	if camera_input != 1:
-		zoom = zoom.lerp(Vector2(camera_input, camera_input), delta).clamp(Vector2(.25, .25), Vector2(1, 1))
+	position += input_vector*delta*4000
 	
 	
